89418657702f9e8104369fb796b468f0528cb573
Woody Lee <woody.lee@EF.com>
2017-05-26 18:22:18 +0800

    a simple Slf4Net sample

---
+++ Slf4Net.Sample.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using Slf4Net.Sample.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slf4Net.Sample.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SomeComponent().DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Slf4Net.Sample.Library/SomeComponent.cs
@@ -0,0 +1,23 @@
+﻿using slf4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slf4Net.Sample.Library
+{
+    public class SomeComponent
+    {
+        private static ILogger _logger = LoggerFactory.GetLogger(typeof(SomeComponent));
+
+        public void DoSomething()
+        {
+            _logger.Info("Before executing ...");
+
+            Console.WriteLine("Executing DoSomething");
+
+            _logger.Info("After executing ...");
+        }
+    }
+}
