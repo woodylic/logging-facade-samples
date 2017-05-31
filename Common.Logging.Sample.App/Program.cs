d319e91bb0b3f033891241cc10e8f4bbf871a7d0
Woody Lee <woody.lee@EF.com>
2017-05-31 09:42:55 +0800

    add Common.Logging sample

--- Common.Logging.Sample.App/Program.cs
+++ Common.Logging.Sample.App/Program.cs
@@ -1,17 +1,19 @@
-﻿using Slf4Net.Sample.Library;
+﻿using Common.Logging.Sample.Library;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Slf4Net.Sample.App
+namespace Common.Logging.Sample.App
 {
     class Program
     {
         static void Main(string[] args)
         {
             new SomeComponent().DoSomething();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Common.Logging.Sample.Library/SomeComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace Common.Logging.Sample.Library
+{
+    public class SomeComponent
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(SomeComponent));
+
+        public void DoSomething()
+        {
+            _logger.Info("Before executing ...");
+
+            Console.WriteLine("Executing DoSomething");
+
+            _logger.Info("After executing ...");
+        }
+    }
+}
